29a97a32793bfd821d37091eb2918758b9a2b367
unknown <nastyakudashevaaface@gmail.com>
2017-12-13 01:40:34 +0600

    Личный проект: милый DOM

--- js/map.js
+++ js/map.js
@@ -4,34 +4,10 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var fragment = document.createDocumentFragment();
-var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var types = ['flat', 'house', 'bungalo'];
-var chekins = ['12:00', '13:00', '14:00'];
-var checkouts = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var avas = [1, 2, 3, 4, 5, 6, 7, 8];
-
 function getShuffleArray(array) {
   return array.slice().sort(function () {
     return Math.random() - 0.5;
   });
-}
-
-function getNoRepeatNumber(array) {
-  var arrayElem = getShuffleArray(array)[getRandom(1, getShuffleArray(array).length)];
-  avas = getShuffleArray(array).filter(function (number) {
-    return number !== arrayElem;
-  });
-  return arrayElem;
-}
-
-function getNoRepeatTitle(array) {
-  var arrayElem = getShuffleArray(array)[getRandom(1, getShuffleArray(array).length)];
-  titles = getShuffleArray(array).filter(function (number) {
-    return number !== arrayElem;
-  });
-  return arrayElem;
 }
 
 function getRandomLengthArray(array) {
@@ -42,8 +18,9 @@ function getRandomLengthArray(array) {
   return randFeatures;
 }
 
-var randFeatures = getRandomLengthArray(features);
 function getList() {
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var randFeatures = getRandomLengthArray(features);
   var list = [];
   for (var i = 0; i < getRandom(1, randFeatures.length); i++) {
     list.push('<li class="feature feature--' + randFeatures[i] + '"></li>');
@@ -53,36 +30,42 @@ function getList() {
 
 function getPoints() {
   var points = [];
+  var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+  var types = ['flat', 'house', 'bungalo'];
+  var chekins = ['12:00', '13:00', '14:00'];
+  var checkouts = ['12:00', '13:00', '14:00'];
+  var avas = [1, 2, 3, 4, 5, 6, 7, 8];
+  var shuffleTitles = getShuffleArray(titles);
+  var shuffleAvas = getShuffleArray(avas);
   for (var i = 0; i < 8; i++) {
-    points.push(
-        {
-          location: {
-            x: getRandom(300, 900),
-            y: getRandom(100, 500)
-          },
-          offer: {
-            title: getNoRepeatTitle(titles),
-            price: getRandom(999, 100001),
-            type: types[getRandom(0, types.length)],
-            rooms: getRandom(1, 6),
-            guests: getRandom(1, 1000),
-            checkin: chekins[getRandom(0, chekins.length)],
-            checkout: checkouts[getRandom(0, checkouts.length)],
-            features: getList(),
-            description: '',
-            photos: []
-          },
-          author: {
-            avatar: 'img/avatars/user' + '0' + getNoRepeatNumber(avas) + '.png'
-          }
-        });
+    points.push({
+      location: {
+        x: getRandom(300, 900),
+        y: getRandom(100, 500)
+      },
+      offer: {
+        title: shuffleTitles.pop(),
+        price: getRandom(999, 100001),
+        type: types[getRandom(0, types.length)],
+        rooms: getRandom(1, 6),
+        guests: getRandom(1, 1000),
+        checkin: chekins[getRandom(0, chekins.length)],
+        checkout: checkouts[getRandom(0, checkouts.length)],
+        features: getList(),
+        description: '',
+        photos: []
+      },
+      author: {
+        avatar: 'img/avatars/user' + '0' + shuffleAvas.pop() + '.png'
+      }
+    });
   }
   return points;
 }
 
 var points = getPoints();
 var similarListPins = document.querySelector('.map__pins');
-var similarPinTemplate = document.getElementsByTagName('template')[0].content.querySelector('.map__pin');
+var similarPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 function renderPins(point) {
   var pinElement = similarPinTemplate.cloneNode(true);
   similarPinTemplate.style.left = point.location.x + 'px';
@@ -92,7 +75,7 @@ function renderPins(point) {
 }
 
 var similarListPopups = document.querySelector('.map');
-var similarPopupTemplate = document.getElementsByTagName('template')[0].content.querySelector('article.map__card');
+var similarPopupTemplate = document.querySelector('template').content.querySelector('article.map__card');
 function renderPopup(point) {
   var pointElement = similarPopupTemplate.cloneNode(true);
   similarPopupTemplate.getElementsByTagName('h3')[0].textContent = point.offer.title;
@@ -109,13 +92,15 @@ function renderPopup(point) {
   return pointElement;
 }
 
+var fragment = document.createDocumentFragment();
 for (var k = 0; k < points.length; k++) {
   fragment.appendChild(renderPins(points[k]));
 }
 
 var map = document.querySelector('.map');
-map.insertBefore((renderPopup(points[3])), map.children[1]);
+map.insertBefore((renderPopup(points[0])), map.children[1]);
 
 similarListPins.appendChild(fragment);
 similarListPopups.appendChild(fragment);
-document.querySelector('.map').classList.remove('map--faded');
+
+map.classList.remove('map--faded');
