5e0497a8d80a07027c392feb2a928a23a335b742
unknown <nastyakudashevaaface@gmail.com>
2017-12-24 14:00:39 +0600

    Личный проект: уникальный код

--- js/form.js
+++ js/form.js
@@ -1,16 +1,6 @@
 'use strict';
 
 window.form = (function () {
-  var selectToSync = document.querySelector('#timein');
-  selectToSync.addEventListener('change', function syncTime() {
-    document.getElementById(selectToSync.dataset.syncwith).options[selectToSync.selectedIndex].selected = true;
-  });
-
-  var selectToSync2 = document.querySelector('#timeout');
-  selectToSync2.addEventListener('change', function syncTime() {
-    document.getElementById(selectToSync2.dataset.syncwith).options[selectToSync2.selectedIndex].selected = true;
-  });
-
   var elementPrice = document.querySelector('#type');
   var price = document.querySelector('#price');
   var selectToSync = document.querySelector('#timein');
